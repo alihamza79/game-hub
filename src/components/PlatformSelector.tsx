599b93898e615d918ea1fd78e04339c1db238950
Ali Hamza <hamzachaudhay79@gmail.com>
2024-02-19 10:05:54 +0500

    Build Platform selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,20 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import usePlatform from "../hooks/usePlatform";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatform();
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button}>Platforms</MenuButton>
+      <MenuList>
+        {data?.map((d) => (
+          <MenuItem>{d.name}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
