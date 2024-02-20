8a8024bb40fc13b2383683918e0baf2e00af1bfd
Ali Hamza <hamzachaudhay79@gmail.com>
2024-02-20 17:11:18 +0500

    Handle game with No Images

--- src/services/image-optimize.ts
+++ src/services/image-optimize.ts
@@ -1,8 +1,8 @@
-
+import noImage from "../assets/no-image-placeholder.webp"
 
 const getCroppedImageUrl = (url:string)=>{
     if(!url)
-         return '';
+         return noImage;
     const index = url.indexOf("media/")+"media/".length;
     const newUrdl = url.slice(0,index) +"crop/600/400/" + url.slice(index);
     return newUrdl;
